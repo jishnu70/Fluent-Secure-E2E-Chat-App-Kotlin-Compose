56795c987a3b2099ee3b0f5c5313dc590728196f
jishnuc <jishnuchandru009@gmail.com>
2025-06-17 16:05:43 +0530

    Presentation Layer Chat TopBar Animation Complete

--- app/src/main/java/com/example/fluent/presentation/chatList/ChatListScreen.kt
+++ app/src/main/java/com/example/fluent/presentation/chatList/ChatListScreen.kt
@@ -1,26 +1,46 @@
 package com.example.fluent.presentation.chatList
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fluent.presentation.chatList.components.ChatListItem
 import com.example.fluent.presentation.chatList.components.ChatTopBarRoot
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun ChatListScreenRoot() {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        state = rememberTopAppBarState()
+    )
+    val focusManager = LocalFocusManager.current
+
     Scaffold(
         modifier = Modifier
-            .fillMaxSize() ,
-        topBar = { ChatTopBarRoot() },
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            },
+        topBar = { ChatTopBarRoot(scrollBehavior = scrollBehavior) },
     ) { innerPadding ->
         Surface(
             modifier = Modifier
@@ -34,10 +54,14 @@ fun ChatListScreenRoot() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ChatList() {
     LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp)
     ) {
         items(count = 20) {
             ChatListItem()
--- app/src/main/java/com/example/fluent/presentation/chatList/components/ChatTopBar.kt
+++ app/src/main/java/com/example/fluent/presentation/chatList/components/ChatTopBar.kt
@@ -3,29 +3,31 @@ package com.example.fluent.presentation.chatList.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.fluent.R
 import com.example.fluent.ui.theme.MatteWhiteGrayMedium
@@ -33,78 +35,77 @@ import com.example.fluent.ui.theme.Orange
 import com.example.fluent.ui.theme.Peach
 import com.example.fluent.ui.theme.PeachWhite
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ChatTopBarRoot() {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth(),
-        color = PeachWhite,
-        shape = RoundedCornerShape(bottomStart = 30.dp, bottomEnd = 30.dp),
-        shadowElevation = 10.dp
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 20.dp),
-            verticalArrangement = Arrangement.SpaceBetween,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            ChatTopBar()
-            Spacer(modifier = Modifier.height(16.dp))
-            ChatSearchBar()
-        }
-    }
-}
+fun ChatTopBarRoot(scrollBehavior: TopAppBarScrollBehavior) {
 
-@Composable
-private fun ChatTopBar() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Min)
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically
+    Surface(
+        tonalElevation = 12.dp,
+        shadowElevation = 10.dp,
+        shape = RoundedCornerShape(bottomStart = 40.dp, bottomEnd = 40.dp),
     ) {
-        Row {
-            Text(
-                text = "Fluent ",
-                style = MaterialTheme.typography.headlineLarge,
-                fontWeight = FontWeight.W900,
-                color = Orange,
-                textDecoration = TextDecoration.Underline
-            )
-            Text(
-                text = "Chat",
-                style = MaterialTheme.typography.headlineLarge,
-                fontWeight = FontWeight.W400,
-                color = Color.Black,
-                textDecoration = TextDecoration.Underline
-            )
-        }
-        Spacer(modifier = Modifier.width(20.dp))
-        IconButton(
-            onClick = {},
+        TopAppBar(
             modifier = Modifier
-                .size(50.dp)
-                .background(
-                    MatteWhiteGrayMedium,
-                    RoundedCornerShape(45)
-                )
-                .shadow(
-                    elevation = 10.dp,
-                    RoundedCornerShape(45),
-                    ambientColor = Peach,
-                    spotColor = PeachWhite
-                )
-                .padding(8.dp)
-        ) {
-            Icon(
-                painter = painterResource(R.drawable.new_message),
-                contentDescription = "New Message",
-                tint = Color.Unspecified,
-            )
-        }
+                .clip(RoundedCornerShape(bottomStart = 40.dp, bottomEnd = 40.dp)),
+            title = {
+                Column(
+                    modifier = Modifier.padding(bottom = 10.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Row {
+                            Text(
+                                text = "Fluent ",
+                                style = MaterialTheme.typography.headlineLarge,
+                                fontWeight = FontWeight.W900,
+                                color = Orange,
+                                textDecoration = TextDecoration.Underline
+                            )
+                            Text(
+                                text = "Chat",
+                                style = MaterialTheme.typography.headlineLarge,
+                                fontWeight = FontWeight.W400,
+                                color = Color.Black,
+                                textDecoration = TextDecoration.Underline
+                            )
+                        }
+                        Spacer(modifier = Modifier.width(20.dp))
+                        IconButton(
+                            onClick = {},
+                            modifier = Modifier
+                                .size(50.dp)
+                                .background(
+                                    MatteWhiteGrayMedium,
+                                    RoundedCornerShape(45)
+                                )
+                                .shadow(
+                                    elevation = 10.dp,
+                                    RoundedCornerShape(45),
+                                    ambientColor = Peach,
+                                    spotColor = PeachWhite
+                                )
+                                .padding(8.dp)
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.new_message),
+                                contentDescription = "New Message",
+                                tint = Color.Unspecified,
+                            )
+                        }
+                    }
+                    ChatSearchBar()
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(containerColor = PeachWhite),
+            scrollBehavior = scrollBehavior,
+            expandedHeight = TopAppBarDefaults.MediumAppBarExpandedHeight
+        )
     }
 }
